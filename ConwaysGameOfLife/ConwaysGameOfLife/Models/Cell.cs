93773ff2aeee3f5862b9b7020d1f0b18ba99dbc8
Michael Veitch <mikeveitch09876@gmail.com>
2017-09-27 11:03:22 +0200

    Added Base Project

---
+++ ConwaysGameOfLife/ConwaysGameOfLife/Models/Cell.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConwaysGameOfLife.Models
+{
+    public class Cell
+    {
+        public bool IsCellAlive { get; set; }
+    }
+}
